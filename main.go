75b9f25364f76fc4d4426d9a681f996661bc179c
Sevastian Zare <sevastian.zare@knf.stud.vu.lt>
2022-11-15 02:06:13 +0200

    cl args; time package, compile speed test

--- main.go
+++ main.go
@@ -6,9 +6,46 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
+
+func echo1() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(os.Args[0], s)
+}
+
+func echo2() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(os.Args[0], s)
+}
+
+func echo3() {
+	fmt.Println(os.Args[0], strings.Join(os.Args[1:], " "))
+}
 
 // efficent way to do it
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	start := time.Now()
+	echo1()
+	secs := time.Since(start).Seconds()
+	fmt.Println(secs)
+
+	start = time.Now()
+	echo2()
+	secs = time.Since(start).Seconds()
+	fmt.Println(secs)
+
+	start = time.Now()
+	echo3()
+	secs = time.Since(start).Seconds()
+	fmt.Println(secs)
+
 }
