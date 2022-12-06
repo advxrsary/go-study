71e1026ff45b455642113786c388998c6c950f1b
Sevastian Zare <sevastian.zare@knf.stud.vu.lt>
2022-12-06 02:51:53 +0200

    main upd

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"go-study/hash"
-	"go-study/loading_anim"
+	"fmt"
 )
 
 func main() {
-	loading_anim.LoadAnim(200)
-	hash.GenerateAvatar()
+	fmt.Println("Hello, world!")
 }
